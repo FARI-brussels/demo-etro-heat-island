33d980e141fcf4ef27d91c2cbf5aba05e736dc3b
m0satron <msebdani@gmail.com>
2025-08-21 11:18:03 +0200

    fix: change default value to summer_day

--- frontend/src/views/InteractiveView.vue
+++ frontend/src/views/InteractiveView.vue
@@ -67,7 +67,7 @@ import WebcamDisplay from '../components/WebcamDisplay.vue';
 import WeatherItem from '../components/WeatherItem.vue';
 import HeatmapDisplay from '../components/HeatmapDisplay.vue';
 import { generateHeatmap } from '../utils/heatmap';
-import { FIcon } from 'fari-component-library'
+
 
 interface WeatherData {
   t2m: number;
@@ -92,7 +92,7 @@ const temperature = ref<number | null>(null);
 const minTemp = ref<number | null>(null);
 const maxTemp = ref<number | null>(null);
 const weatherData = ref<WeatherData | null>(null);
-const selectedMode = ref<string>('real_time');
+const selectedMode = ref<string>('summer_day');
 
 const backendUrl = "http://localhost:5000/process_image"
 
