9751a4df9b5c3b8286264625681712572bcd9ccb
m0satron <msebdani@gmail.com>
2025-08-28 11:08:08 +0200

    feat: add cesium map to interactive view

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cesium from 'vite-plugin-cesium'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), cesium()],
     server: {
     proxy: {
       '/process_image': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+      },
+      '/wms': {
+        target: 'https://ows.environnement.brussels/air',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/wms/, ''),
       },
     },
   },
