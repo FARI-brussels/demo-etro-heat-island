bfaf52c5c83eb8c7f0e64a340f77ae4ff4343aa3
m0satron <msebdani@gmail.com>
2025-08-28 11:56:09 +0200

    feat: add cms data to heatmap

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="view bg-color-blue">
+    <FButtonIcon
+      name="tooltip"
+      class="tooltip"
+      color="blue-light"
+      small
+      @click="toggleInfoCard"
+    />
     <div class="heading">
       <div class="title"> <FTitle class="title"> HEATMAP BRUSSELS </FTitle> </div>
       </div>
@@ -11,6 +18,51 @@
       @click="router.push('/interactive')"
     />
 
+    <img v-if="data?.image" :src="data.image" alt="Background" class="background-image rounded" />
+<FSlideTransition :show="showInfoCard">
+      <FCard
+        v-if="showInfoCard"
+        @close="toggleInfoCard"
+        @update:locale="setLocale"
+        class="card"
+      >
+        {{ data.description[locale] }}
+
+        <div class="researchers-container">
+          <span class="researchers">
+            research head:
+            <span class="research-head color-black">
+              {{ data.research_head }}
+            </span>
+          </span>
+          <span class="researchers">
+            research lead:
+            <span class="research-lead color-black">
+              {{ data.research_lead }}
+            </span>
+          </span>
+          <div class="flex">
+            <img
+              v-for="sdg in data.sdg_images"
+              :key="sdg"
+              :src="sdg"
+              class="sdg mr-md mt-sm"
+            />
+          </div>
+        </div>
+        <template #footer>
+          <div v-if="data?.logos">
+            <img
+              v-for="logo in data.logos"
+              :src="logo"
+              :key="logo"
+              class="card-logo"
+            />
+          </div>
+        </template>
+      </FCard>
+    </FSlideTransition>
+    <div class="backdrop" :class="{ 'backdrop-active': showInfoCard }"></div>
     <FFooter class="footer" />
   </div>
 
@@ -22,12 +74,26 @@ import {
   FTitle,
   FFooter,
   FButton,
-
+  FButtonIcon,
+  FSlideTransition,
+  FCard,
 } from 'fari-component-library'
-
 import { useRouter } from 'vue-router'
+import { useDataStore } from '@/stores/cms'
+import { ref, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+
+
+const showInfoCard = ref(false)
+const toggleInfoCard = () => (showInfoCard.value = !showInfoCard.value)
+
+const { data, locale } = storeToRefs(useDataStore())
+const { getData, setLocale } = useDataStore()
+
+onMounted(getData)
 
 const router = useRouter()
+
 </script>
 
 <style scoped lang="scss">
@@ -39,6 +105,54 @@ const router = useRouter()
   width: 100%;
   position: relative;
 }
+
+.tooltip {
+  z-index: 2;
+  position: absolute;
+  top: 7rem;
+  right: 32rem;
+}
+.card {
+  position: absolute;
+  top: 20%;
+  left: 20%;
+  z-index: 3;
+}
+
+.background-image {
+  width: 50vw;
+  position: absolute;
+  top: 25%;
+  left: 25%;
+  z-index: 1;
+}
+
+.researchers-container {
+  margin-top: auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.researchers {
+  color: #888;
+  text-transform: uppercase;
+}
+
+.research-head,
+.research-lead {
+  text-transform: none;
+}
+
+.card-logo {
+  height: 3.5rem;
+  margin-right: 2rem;
+}
+
+.sdg {
+  height: 3.5rem;
+  width: 3.5rem;
+}
+
 
 
 .heading {
@@ -72,6 +186,27 @@ const router = useRouter()
   position: absolute;
   bottom: 0;
   width: 100%;
+}
+
+.backdrop {
+  visibility: hidden;
+  opacity: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(0);
+  z-index: 2;
+  transition: all 100ms;
+
+  &-active {
+    visibility: visible;
+    opacity: 1;
+    background-color: rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(2px);
+    transition: all 300ms;
+  }
 }
 
 </style>
