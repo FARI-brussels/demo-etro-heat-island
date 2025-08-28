af751e5bbb79de1d880234d4088b47fea5ff6c8c
m0satron <msebdani@gmail.com>
2025-08-28 14:51:34 +0200

    feat: add store

---
+++ frontend/src/stores/cms.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import { fetchDirectus } from 'fari-directus-parser'
+
+
+type Locale = 'en' | 'fr-FR' | 'fr' | 'nl'
+
+export const useDataStore = defineStore('data', () => {
+  const locale = ref<Locale>('en')
+  const data = ref(null)
+  const loading = ref(false)
+  const error = ref(null)
+
+  async function getData() {
+    data.value = await fetchDirectus({ slug: 'heatmap' })
+    
+    console.log('Fetched data:', data.value)
+  }
+  const setLocale = (l: Locale) =>
+    l === 'fr-FR' ? (locale.value = 'fr') : (locale.value = l)
+
+  return {
+    locale,
+    setLocale,
+    data,
+    loading,
+    error,
+    getData,
+  }
+})
